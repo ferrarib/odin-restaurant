ea7bfde5174b53532c5daf2f59637472645db468
Brandon Ferrari <28712132+ferrarib@users.noreply.github.com>
2023-12-15 19:30:05 -0500

    Beginning of menu page

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,48 @@
 
-export default function() {
-    const title = document.createElement('div');
+let menuItems = [
+    {name: "Pizza", cost: "$12.99"},
+    {name: "Pizza", cost: "$12.99"},
+    {name: "Pizza", cost: "$12.99"},
+    {name: "Pizza", cost: "$12.99"},
+];
 
+function CreateMenuItem(item){
+    const menuItem = document.createElement('div');
+    menuItem.textContent = item.name + " - " + item.cost;
+    return menuItem;
+}
+
+export default function() {
+    const menuBox = document.createElement('div');
+    menuBox.classList.add('menu-box');
+
+    const menuVertical = document.createElement('div');
+    menuVertical.classList.add('menu-vertical');
+    const menuHorizontal = document.createElement('div');
+    menuHorizontal.classList.add('menu-horizontal');
+
+    menuBox.appendChild(menuHorizontal);
+    menuBox.appendChild(menuVertical);
+
+    const title = document.createElement('div');
+    title.classList.add('menu-title');
     title.textContent = "Menu";
-    return title;
+
+    menuBox.appendChild(title);
+
+    const hr = document.createElement('hr');
+    hr.classList.add('menu-hr');
+
+    menuBox.appendChild(hr);
+
+    const menuItemsContainer = document.createElement('div');
+    menuItemsContainer.classList.add('menu-items-container');
+
+    menuItems.forEach((item) => {
+        menuItemsContainer.appendChild(CreateMenuItem(item));
+    });
+
+    menuBox.appendChild(menuItemsContainer);
+
+    return menuBox;
 }
