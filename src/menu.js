46630ef4dc76642a745ec03ff8a769b7d171ec72
Brandon Ferrari <28712132+ferrarib@users.noreply.github.com>
2023-12-15 23:01:44 -0500

    Finished Menu Page

--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,62 @@
 
 let menuItems = [
-    {name: "Pizza", cost: "$12.99"},
-    {name: "Pizza", cost: "$12.99"},
-    {name: "Pizza", cost: "$12.99"},
-    {name: "Pizza", cost: "$12.99"},
+    {
+        heading: "Appetizers",
+        items: [
+            { name: "Garlic Bread", cost: "$7.99" },
+            { name: "Bruschetta", cost: "$8.99" },
+            { name: "Calamari", cost: "$10.99" },
+            { name: "Quesadillas", cost: "$12.99" }
+        ]
+    },
+    {
+        heading: "Drinks",
+        items: [
+            { name: "Merlot", cost: "$7.99" },
+            { name: "Long Island Iced Tea", cost: "$9.99" },
+            { name: "Sangria", cost: "$7.99" },
+            { name: "White Russian", cost: "$10.99" }
+        ]
+    },
+    {
+        heading: "Entrees",
+        items: [
+            { name: "Sirloin Steak", cost: "$20.99" },
+            { name: "Lobster", cost: "$24.99" },
+            { name: "Chicken Parmasan", cost: "$18.99" },
+            { name: "Pizza", cost: "$17.99" }
+        ]
+    },
+    {
+        heading: "Desserts",
+        items: [
+            { name: "Carrot Cake", cost: "$12.99" },
+            { name: "Chocolate Lava Cake", cost: "$11.99" },
+            { name: "Pumpkin Pie", cost: "$9.99" },
+            { name: "Apple Crumble", cost: "$8.99" }
+        ]
+    }
 ];
 
-function CreateMenuItem(item){
+function CreateMenuItem(item) {
     const menuItem = document.createElement('div');
-    menuItem.textContent = item.name + " - " + item.cost;
+    menuItem.classList.add('menu-item');
+
+    const menuName = document.createElement('div');
+    menuName.textContent = item.name;
+
+    const menuDivide = document.createElement('hr');
+
+    const menuCost = document.createElement('div');
+    menuCost.textContent = item.cost;
+
+    menuItem.appendChild(menuName);
+    menuItem.appendChild(menuDivide);
+    menuItem.appendChild(menuCost);
     return menuItem;
 }
 
-export default function() {
+export default function () {
     const menuBox = document.createElement('div');
     menuBox.classList.add('menu-box');
 
@@ -39,7 +83,20 @@ export default function() {
     menuItemsContainer.classList.add('menu-items-container');
 
     menuItems.forEach((item) => {
-        menuItemsContainer.appendChild(CreateMenuItem(item));
+        const section = document.createElement('div');
+        section.classList.add('menu-section');
+        
+        const sectionTitle = document.createElement('div');
+        sectionTitle.classList.add('menu-section-title');
+        sectionTitle.textContent = item.heading;
+
+        section.appendChild(sectionTitle);
+
+        item.items.forEach((menuItem) => {
+            section.appendChild(CreateMenuItem(menuItem));
+        });
+
+        menuItemsContainer.appendChild(section);
     });
 
     menuBox.appendChild(menuItemsContainer);
