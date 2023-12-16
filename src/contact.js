bca66238898ac5e25c5e5077f873986d53cde77b
Brandon Ferrari <28712132+ferrarib@users.noreply.github.com>
2023-12-16 01:25:48 -0500

    Contact Page done and full website finished

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,66 @@
 
 export default function() {
-    const title = document.createElement('div');
+    const contactBox = document.createElement('div');
+    contactBox.classList.add('contact-box');
 
-    title.textContent = "Contact";
-    return title;
+    const contactForm = document.createElement('form');
+    contactForm.classList.add('contact-form');
+
+    //Name Field
+    const nameField = document.createElement('div');
+    const labelName = document.createElement('label');
+    labelName.setAttribute('for', 'name');
+    labelName.textContent = "Name: ";
+    const inputName = document.createElement('input');
+    inputName.setAttribute('name', 'name');
+    inputName.setAttribute('id', 'name');
+
+    nameField.appendChild(labelName);
+    nameField.appendChild(inputName);
+
+    contactForm.appendChild(nameField);
+
+    //Email Field
+    const emailField = document.createElement('div');
+    const labelEmail = document.createElement('label');
+    labelEmail.setAttribute('for', 'email');
+    labelEmail.textContent = "Email: ";
+    const inputEmail = document.createElement('input');
+    inputEmail.setAttribute('name', 'email');
+    inputEmail.setAttribute('id', 'email');
+
+    emailField.appendChild(labelEmail);
+    emailField.appendChild(inputEmail);
+
+    contactForm.appendChild(emailField);
+
+    //Message Field
+    const messageField = document.createElement('div');
+    const labelMessage = document.createElement('label');
+    labelMessage.setAttribute('for', 'message');
+    labelMessage.textContent = "Message: ";
+    const inputMessage = document.createElement('textarea');
+    inputMessage.setAttribute('name', 'message');
+    inputMessage.setAttribute('id', 'message');
+    inputMessage.setAttribute('rows', '4');
+
+    messageField.appendChild(labelMessage);
+    messageField.appendChild(inputMessage);
+
+    contactForm.appendChild(messageField);
+
+    //Submit Button
+    const submit = document.createElement('button');
+    submit.textContent = "Submit";
+    submit.classList.add('submit-button');
+    
+    contactForm.appendChild(submit);
+    contactForm.onsubmit = (e) => {
+        e.preventDefault();
+    }
+
+
+    contactBox.appendChild(contactForm);
+
+    return contactBox;
 }
